73f805c8eab90c7652efec70c949f5464cffa3f0
amishra9235 <anand.mishra9235@gmail.com>
2025-01-14 20:49:18 +0530

    first commit

---
+++ src/test/java/jenkinsLearn/TestOne.java
@@ -0,0 +1,39 @@
+package jenkinsLearn;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestOne {
+	
+	@Test
+	public void testOne()
+	{
+		Assert.assertTrue(false);
+		
+	}
+
+	
+	@Test
+	public void testTwoOne()
+	{
+		Assert.assertTrue(true);
+		
+	}
+
+	
+	@Test
+	public void testThreeOne()
+	{
+		Assert.assertTrue(false);
+		
+	}
+
+	
+	@Test
+	public void testfourOne()
+	{
+		Assert.assertTrue(true);
+		
+	}
+
+}
